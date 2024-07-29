db5533415c47d35355a8420fcd5477265af142f2
Tuan <2200001268@nttu.edu.vn>
2024-07-29 16:37:11 +0700

    newfile

--- pages/main/lienhe.php
+++ pages/main/lienhe.php
@@ -1,4 +1,8 @@
 <p>Lien He</p>
+<h2>
+    <td>Huỳnh Giang Hữu Tuấn</td>
+    <tr a href="tef:070085106">Sđt</tr>
+</h2>
 <?php
     $sql_lh = "SELECT * FROM tbl_lienhe WHERE id=1";
     $query_lh = mysqli_query($mysqli,$sql_lh);
